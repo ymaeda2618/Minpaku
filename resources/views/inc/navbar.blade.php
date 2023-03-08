06e4e1730af1cf92ba73553abf13b79cf6e181a1
y_maeda <y.maeda2618@gmail.com>
2023-03-08 17:04:05 +0900

    トップに戻るリンク修正

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,7 +7,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-            <a class="navbar-brand" href="/"><img class="pc-zone" src="{{ asset('/img/letter_logo.png') }}" alt="軽井沢 紫水京" /></a>
+            <a class="navbar-brand" href="{{ route('home') }}"><img class="pc-zone" src="{{ asset('/img/letter_logo.png') }}" alt="軽井沢 紫水京" /></a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
